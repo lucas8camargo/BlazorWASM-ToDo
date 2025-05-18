0f089fd562cc81fae3286745a913e85f59ca3be3
Lucas Camargo <lucas8camargo@protonmail.com>
2025-05-18 17:33:09 -0300

    Blazor WASM ToDo

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorToDoWASM.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ Services/ToDoService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorToDoWASM.Models;
+
+namespace BlazorToDoWASM.Services
+{
+    public class ToDoService
+    {
+        private List<ToDoItem> items = new();
+        private int nextId = 1;
+
+        public List<ToDoItem> GetAll() => items;
+
+        public void Add(string title)
+        {
+            items.Add(new ToDoItem { Id = nextId++, Title = title, IsDone = false });
+        }
+
+        public void Update(ToDoItem item)
+        {
+            var existing = items.FirstOrDefault(x => x.Id == item.Id);
+            if (existing != null)
+            {
+                existing.Title = item.Title;
+                existing.IsDone = item.IsDone;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var item = items.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+                items.Remove(item);
+        }
+    }
+}
